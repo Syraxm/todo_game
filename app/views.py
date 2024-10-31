0fa0967fe63bfe5477eb91072ffc8fd168711c64
Syraxm <tyomachannel@gmail.com>
2024-10-31 16:48:48 +0300

    added the ability to delete todo

--- app/views.py
+++ app/views.py
@@ -2,24 +2,35 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Todo
 from . import db
 
-main = Blueprint('main', __name__)
+todo = Blueprint('main', __name__)
 
 
-@main.route('/')
+@todo.route('/')
 def home():
     todo_list = Todo.query.all()
     return render_template('home.html', todo_list=todo_list)
 
 
-@main.route('/add_todo')
+@todo.route('/add_todo')
 def add_todo():
     todo_title = request.args.get('todo_title')
     new_todo = Todo(title=todo_title, complete=False)
 
     db.session.add(new_todo)
     db.session.commit()
+
+    return redirect(url_for('main.home')) # todo: refresh page
+
+
+@todo.route('/delete_todo/<todo_id>')
+def delete_todo(todo_id: int):
+    todo = Todo.query.filter_by(id = todo_id).first()
+
+    db.session.delete(todo)
+    db.session.commit()
+
     return redirect(url_for('main.home'))
 
 
 def register_routes(app):
-    app.register_blueprint(main)
+    app.register_blueprint(todo)
